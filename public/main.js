75e78ffaa8142a8f6227124e0713e08a75f87383
Achegnui <achegnuijuven@gmail.com>
2024-11-08 16:46:46 +0100

    fixed issue with hamburger

--- public/main.js
+++ public/main.js
@@ -1,53 +1,48 @@
-const menu = document.getElementById("menu")
-const hamburger = document.querySelector(".fa-bars")
+const menu = document.getElementById("menu");
+const hamburger = document.querySelector(".fa-bars");
 
-function onToggleMenu(e){
-    e.name = e.name === hamburger ? "fa-times" : hamburger
-}
-hamburger.addEventListener("click",() =>{
-    menu.classList.toggle("active")
-    if (active){
-        hamburger.classList.replace("fa-bars","fa-times")
-    }
-})
+hamburger.addEventListener("click", () => {
+  menu.classList.toggle("active");
+  hamburger.classList.toggle("fa-bars");
+  hamburger.classList.toggle("fa-times");
+});
 
-const visibility = document.querySelector(".fa-eye-slash")
-const password = document.getElementById("password")
+const visibility = document.querySelector(".fa-eye-slash");
+const password = document.getElementById("password");
 
-visibility.addEventListener("click", () =>{
-    if (password.type === 'password'){
-        password.type = 'text'
-        visibility.classList.replace('fa-eye-slash','fa-eye')
-
-    }else{
-        password.type = 'password' 
-        visibility.classList.replace('fa-eye','fa-eye-slash')
-
-
-    }
-})
+visibility.addEventListener("click", () => {
+  if (password.type === "password") {
+    password.type = "text";
+    visibility.classList.replace("fa-eye-slash", "fa-eye");
+  } else {
+    password.type = "password";
+    visibility.classList.replace("fa-eye", "fa-eye-slash");
+  }
+});
 
 //   count down timer
 
-var countdowndate = new Date("Nov 20, 2024 00:00:00").getTime()
-var x = setInterval(function(){
-    var now = new Date().getTime()
-    var distance = countdowndate - now
+var countdowndate = new Date("Nov 20, 2024 00:00:00").getTime();
+var x = setInterval(function () {
+  var now = new Date().getTime();
+  var distance = countdowndate - now;
 
+  var hours = Math.floor(distance / (1000 * 60 * 60)) + "h";
+  if (hours < 10) {
+    hours = "0" + hours;
+  }
 
-    var hours = (Math.floor(distance / (1000 * 60 * 60)) + "h")
-    if(hours < 10){ hours = "0" + hours}
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60)) + "m";
+  if (minutes < 10) {
+    minutes = "0" + minutes;
+  }
 
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000) + "s";
+  if (seconds < 10) {
+    seconds = "0" + seconds;
+  }
 
-    var minutes = (Math.floor((distance % (1000 *60 * 60)) / (1000 * 60)) + "m")
-    if(minutes < 10){ minutes = "0" + minutes}
-
-
-    var seconds = (Math.floor((distance % (1000 * 60) / 1000 )) + "s")
-    if(seconds < 10){ seconds = "0" + seconds}
-
-    document.getElementById("hour").innerHTML = hours
-    document.getElementById("minute").innerHTML = minutes
-    document.getElementById("second").innerHTML = seconds
-
-},)
+  document.getElementById("hour").innerHTML = hours;
+  document.getElementById("minute").innerHTML = minutes;
+  document.getElementById("second").innerHTML = seconds;
+});
